48805b422abf74893a1ef139b0005e4c33f07544
Piotr <bednarek.piotr@vp.pl>
2018-07-13 01:49:44 +0200

    initial commit

---
+++ src/main/java/com/bednarekpiotr/ManagingLessonsSystemApplication.java
@@ -0,0 +1,12 @@
+package com.bednarekpiotr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagingLessonsSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagingLessonsSystemApplication.class, args);
+    }
+}
---
+++ src/main/java/com/bednarekpiotr/controllers/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.bednarekpiotr.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "index";
+    }
+}
