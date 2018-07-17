54b20c940ed9b37d053ba2bbecadabe2d867e2c6
Piotr <bednarek.piotr@vp.pl>
2018-07-17 19:12:06 +0200

    Configurations

--- src/main/java/com/bednarekpiotr/controllers/WelcomeController.java
+++ src/main/java/com/bednarekpiotr/controllers/WelcomeController.java
@@ -1,13 +1,23 @@
 package com.bednarekpiotr.controllers;
 
+import java.time.LocalDate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WelcomeController {
 
-    @RequestMapping("/")
-    public String home() {
-        return "index";
-    }
+  @RequestMapping("/")
+  public String home() {
+    return "index";
+  }
+
+  @GetMapping("/greetings")
+  public String greeting(Model model) {
+    LocalDate localDate = LocalDate.now();
+    model.addAttribute("date", localDate);
+    return "greetings";
+  }
 }
